9b14a33ac0dfe1a23453c9d2a3f613a7f59b9d14
Phoenix787 <shiny777@bk.ru>
2016-06-17 09:23:31 +0500

    new changes

--- src/main/java/ru/javastudy/AppParentChild.java
+++ src/main/java/ru/javastudy/AppParentChild.java
@@ -6,6 +6,7 @@ import ru.javastudy.entity.Parent;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import java.util.List;
 
 /**
  * Created by Сергеева on 16.06.2016.
@@ -15,27 +16,28 @@ public class AppParentChild {
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("jpa_demo");
         EntityManager entityManager = entityManagerFactory.createEntityManager();
 
-        Parent parent_1 = entityManager.find(Parent.class, 1);
+//        Parent parent_1 = entityManager.find(Parent.class, 1);
+////        entityManager.getTransaction().begin();
+////        entityManager.persist(parent_1);
+////        entityManager.getTransaction().commit();
+//
+//        //System.out.println(entityManager.find(Parent.class, 1).getCaption());
+//
+//        Child child_500 = new Child();
+//        child_500.setParent(parent_1);
+//        child_500.setCaption("child_100000123456500!!!");
+//
+//        parent_1.getChildren().add(child_500);
+//
 //        entityManager.getTransaction().begin();
-//        entityManager.persist(parent_1);
+//        entityManager.persist(child_500);
 //        entityManager.getTransaction().commit();
 
-        //System.out.println(entityManager.find(Parent.class, 1).getCaption());
+        List<Child> children = entityManager.find(Parent.class, 1).getChildren();
 
-        Child child_500 = new Child();
-        child_500.setParent(parent_1);
-        child_500.setCaption("child_100000123456500!!!");
-
-        parent_1.getChildren().add(child_500);
-
-        entityManager.getTransaction().begin();
-        entityManager.persist(child_500);
-        entityManager.getTransaction().commit();
-
-        System.out.println(entityManager.find(Parent.class, 1).getChildren().get(0).getCaption());
-
-
-
+        for (int i = 0; i < children.size(); i++) {
+            System.out.println(children.get(i).getCaption());
+        }
 
 
         entityManager.close();
